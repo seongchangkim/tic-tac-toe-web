984e8c4c21705b890d0830c6c09176c782debbd5
DunKim <darkgx21@gmail.com>
2023-10-06 10:03:32 +0900

    코드 정리

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,8 @@
-import {
-    Body,
-    Controller,
-    Get,
-    Post,
-    Req,
-    UnauthorizedException,
-    UseGuards,
-    ValidationPipe,
-} from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpForm } from './dto/sign_up_form';
-import {
-    AuthCheckRes,
-    IsSuccess,
-    LoginRes,
-    RequestWithUser,
-} from './type/auth_common_type';
+import { IsSuccess, LoginRes } from './type/auth_common_type';
 import { LoginForm } from './dto/login_form';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -40,23 +25,6 @@ export class AuthController {
             accessToken,
             user,
             isAuth,
-        };
-    }
-
-    @UseGuards(AuthGuard())
-    @Get('/check')
-    checkAuth(@Req() req: RequestWithUser): AuthCheckRes {
-        if (!req.user) throw new UnauthorizedException('로그인이 필요합니다');
-
-        const { user_id, auth_role, nickname, tel } = req.user;
-        return {
-            user: {
-                userId: user_id,
-                nickname,
-                tel,
-                role: auth_role,
-            },
-            isAuth: true,
         };
     }
 }
