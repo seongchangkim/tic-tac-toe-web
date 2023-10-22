b82ab41034fc514e1acc91641834af9371aa5f87
DunKim <darkgx21@gmail.com>
2023-10-22 11:19:53 +0900

    [프론트엔드 기능 구현] 관리자 페이지 Http 상태값 401인 경우 로그인 페이지이동

--- web_client/src/api/call_rest_api.ts
+++ web_client/src/api/call_rest_api.ts
@@ -1,16 +1,13 @@
 import { defaultApiHost } from '../global/default-api-url';
 import axios from 'axios';
-import {
-    CallGetApiParameterType,
-    CallPostApiParameterType,
-} from './parameter_type/parameter_common_type';
+import { CallGetApiParameterType } from './parameter_type/parameter_common_type';
+import router from '@/router';
 
 // GET 방식
-export const callGetApi = async ({
-    path,
-    queryParameter = '',
-    token,
-}: CallGetApiParameterType) => {
+export async function callGetApi(
+    this: any,
+    { path, queryParameter = '', token }: CallGetApiParameterType,
+) {
     try {
         const res = await axios.get(
             `${defaultApiHost}${path}?${queryParameter}`,
@@ -23,13 +20,14 @@ export const callGetApi = async ({
 
         return res.data;
     } catch (e: any) {
-        const { status, message } = e.response.data;
+        const { message, statusCode } = e.response.data;
 
-        if (status >= 403) {
-            alert(message);
+        if (statusCode >= 400) {
+            alert(`${message} : 관리자 계정으로 로그인하세요.`);
+            router.push('/auth/login');
         }
     }
-};
+}
 
 // POST 방식
 // 추후에 구현할 예정임.
